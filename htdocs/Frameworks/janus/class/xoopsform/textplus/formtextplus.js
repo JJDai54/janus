e31148960261c5195ccd3f404aae2957544afbe1
JJDai54 <jjdelalandre@orange.fr>
2025-06-04 22:11:43 +0200

    maj xform formtestplus

--- htdocs/Frameworks/janus/class/xoopsform/textplus/formtextplus.js
+++ htdocs/Frameworks/janus/class/xoopsform/textplus/formtextplus.js
@@ -4,12 +4,13 @@ function XoopsFormTextPlus_clear(event, xfId){
 }
 
 function XoopsFormTextPlus_setValue(event, xfId, exp){
+//console.log(`XoopsFormTextPlus_setValue : exp = ${exp}`);
     document.getElementById(xfId).value = exp;
     document.getElementById(xfId).focus();
 }
 
 function XoopsFormTextPlus_setValueTxt(ev, xfId){
-//alert(ev.currentTarget.id + '===>' + ev.currentTarget.value);
+//console.log(`XoopsFormTextPlus_setValueTxt : exp = ${ev.currentTarget.value}`);
     document.getElementById(xfId).value = ev.currentTarget.value;
     document.getElementById(xfId).focus();
     ev.currentTarget.value = '';
